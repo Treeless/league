bbedb1d417566dd071e4c64f1739afe98d63af4f
Matthew Rowlandson <matt@fyrespark.com>
2018-08-21 04:58:55 -0400

    move api logic to lib folder

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 (function() {
     // Author: Matthew Rowlandson
     // Purpose: Server side for league of legends application
-    const Express = require("express");
-    const http = require('http');
+    const express = require("express");
+    const bodyParser = require("body-parser")
 
     const PORT = 8080;
-    const API = ""; //TODO (waiting for game client)
-    const API_KEY = ""; //Hardcoded for DEMO ONLY
 
-    const app = Express();
+    const app = express();
+    const leagueLib = require("./lib/league.lib.js");
+
+    app.use(bodyParser.json()); //JSON middleman
 
     //Route to get user match statistics from League of legends API
     // NOTE: in order to use the API, you have to install the game client and pick a summoner name! DOH (Takes a bit to DL)
-    app.get("/account/:username", function(req, res) {
-        var username = req.params.username;
+    app.get("/matches/:summonerName", function(req, res) {
+        var summonerName = req.params.summonerName;
 
-        http.get(API + "?api_key=" + API_KEY, (resp) => {
-            let data = '';
-
-            //data from api (one piece at a time...);
-            resp.on('data', (chunk) => {
-                data += chunk;
+        // FIRST: make a call to get the user's accountId
+        leagueLib.getAccountBySummonerName(summonerName, function(err, account) {
+            // SECOND: Now, using the accountId, get list of matches (10)
+            leagueLib.getMatchesByAccountId(account.accountId, function(err, matches) {
+                res.json({ account: account, matches: matches });
             });
-
-            // All the data was retreived from league api
-            resp.on('end', () => {
-                console.log(JSON.parse(data));
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
         });
     });
 
